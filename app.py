51ca8f237a1de94b5d142aff49389502fce51be0
DIVYA <22.divyamahadeva@gmail.com>
2026-06-04 11:00:46 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Student Analytics",
+    page_icon="🤖",
+    layout="wide"
+)
+
+st.title("🤖 AI Impact on Student Life Analytics Platform")
+
+st.markdown("""
+### Insights Generated From 1500 Student Records
+
+Navigate using the sidebar.
+""")
+
+st.image(
+    "https://images.unsplash.com/photo-1677442136019-21780ecad995",
+    use_container_width=True
+)
